f23456e2ce749cf712f6c62bd8fb34b7ef32d8c9
issam <issam@RPC-2RVWMQ2>
2025-01-29 21:08:42 -0500

    "Added couples of line as a test."

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -5,6 +5,13 @@ public class DemoTest4 {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Java");
+		
+		// Variable
+		String country = "USA";
+		String city = "Raleigh";
+		
+		System.out.println("This is just a test - Pushing this Code to Github");
+
 	}
 
 }
